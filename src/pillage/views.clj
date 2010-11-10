9147fc50864b36c3279f599374822c105183b149
Lyle Hanson <lyle2.0@gmail.com>
2010-11-09 22:11:39 -0600

    Added header and sample CSS layout

--- src/pillage/views.clj
+++ src/pillage/views.clj
@@ -2,15 +2,20 @@
   (:use hiccup.core
         hiccup.page-helpers))
 
+(def html-header
+  '([:h1 "Pillage!"
+    [:img {:src "/images/rss-symbol.jpg"}]]))
+
 (defn html-doc
   "Base template for generating an HTML document"
-  ([] (html-doc "Pillage RSS" [:h1 "Hello World!"]))
+  ([] (html-doc "Pillage RSS" html-header ))
   ([title & body]
     (html
       (doctype :xhtml-strict)
       (xhtml-tag "en"
-        [:head
+        [:head (include-css "/css/layout.css")
           [:meta {:http-equiv "Content-type"
                   :content "text/html; charset=utf-8"}]
           [:title title]]
         [:body body]))))
+
